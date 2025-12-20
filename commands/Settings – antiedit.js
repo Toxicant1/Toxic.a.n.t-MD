87beb1df021c4dfcb84db87f9f93b8489ac965e3
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:27:40 +0300

    Delete commands/Settings – antiedit.js

--- commands/Settings – antiedit.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "antiedit",
-    description: "Toggle anti-edit messages",
-    execute: async (msg, args, bot) => {
-        bot.settings.antiedit = !bot.settings.antiedit;
-        return `✅ Anti-edit is now ${bot.settings.antiedit ? "ON" : "OFF"}`;
-    }
-};
