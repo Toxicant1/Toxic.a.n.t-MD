e620b31837b6e457623a8100ff189dac4cf7ecf5
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:24:51 +0300

    Delete commands/Settings – welcomegoodbye.js

--- commands/Settings – welcomegoodbye.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "welcomegoodbye",
-    description: "Toggle welcome/goodbye messages",
-    execute: async (msg, args, bot) => {
-        bot.settings.welcomegoodbye = !bot.settings.welcomegoodbye;
-        return `✅ Welcome/Goodbye messages are now ${bot.settings.welcomegoodbye ? "ON" : "OFF"}`;
-    }
-};
