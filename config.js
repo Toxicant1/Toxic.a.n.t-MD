6e0c29c4640b2b25abeb3c84402d03fa1e844aca
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 09:05:56 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ownerNumber: "YOUR_NUMBER_HERE",   // WhatsApp number for owner (with country code)
+    prefix: ".",                        // Bot command prefix
+    mode: "public",                     // public/private
+    port: 3000                          // pairing web server port
+};
