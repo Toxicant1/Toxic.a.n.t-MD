d8e31ecea2d4c102cb9e28aa1f916d1419eb49a7
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:28:33 +0300

    Delete commands/Settings – prefix.js

--- commands/Settings – prefix.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "prefix",
-    description: "Change bot command prefix",
-    execute: async (msg, args, bot) => {
-        if (!args[0]) return "❌ Usage: .prefix <symbol>";
-        bot.prefix = args[0];
-        return `✅ Prefix changed to: ${bot.prefix}`;
-    }
-};
