bed2ef08ec1249dbaeb3ac884b7c44a05b000feb
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:26:55 +0300

    Delete commands/Settings – antibot.js

--- commands/Settings – antibot.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "antibot",
-    description: "Toggle anti-bot messages",
-    execute: async (msg, args, bot) => {
-        bot.settings.antibot = !bot.settings.antibot;
-        return `✅ Anti-bot is now ${bot.settings.antibot ? "ON" : "OFF"}`;
-    }
-};
