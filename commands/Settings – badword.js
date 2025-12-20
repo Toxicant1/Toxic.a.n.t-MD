b857380ed33cf2f154ecd64a67effc853c9660c9
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:28:09 +0300

    Delete commands/Settings – badword.js

--- commands/Settings – badword.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "badword",
-    description: "Toggle badword filter",
-    execute: async (msg, args, bot) => {
-        bot.settings.badword = !bot.settings.badword;
-        return `✅ Badword filter is now ${bot.settings.badword ? "ON" : "OFF"}`;
-    }
-};
