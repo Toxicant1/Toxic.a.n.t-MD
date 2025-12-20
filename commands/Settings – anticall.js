c3ecd3cd4b353d285bcba3ff2637463399437182
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:27:09 +0300

    Delete commands/Settings – anticall.js

--- commands/Settings – anticall.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "anticall",
-    description: "Toggle anti-call",
-    execute: async (msg, args, bot) => {
-        bot.settings.anticall = !bot.settings.anticall;
-        return `✅ Anti-call is now ${bot.settings.anticall ? "ON" : "OFF"}`;
-    }
-};
