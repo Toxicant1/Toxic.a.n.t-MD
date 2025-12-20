0136891b1c50e82f96d3227f36740603d1617df8
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:27:22 +0300

    Delete commands/Settings – antidelete.js

--- commands/Settings – antidelete.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "antidelete",
-    description: "Toggle anti-delete messages",
-    execute: async (msg, args, bot) => {
-        bot.settings.antidelete = !bot.settings.antidelete;
-        return `✅ Anti-delete is now ${bot.settings.antidelete ? "ON" : "OFF"}`;
-    }
-};
