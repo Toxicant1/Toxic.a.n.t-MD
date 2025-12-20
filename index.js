582f0d28f0488a55fd6d555353824d03d10e0e25
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 20:03:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { startWeb } = require("./pairing");
 
 // Start the clean pairing interface
 // This handles the web UI and the Baileys pairing logic
-console.log("SYSTEM: Initializing Toxicant-MD Web Service...");
+console.log("SYSTEM: Initializing Toxic.a.n.t-MD Web Service...");
 startWeb();
