4f42c9357f54654b8c4ab57d541dd6fe91d67f42
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:27:57 +0300

    Delete commands/Settings – antilink.js

--- commands/Settings – antilink.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "antilink",
-    description: "Toggle anti-link messages",
-    execute: async (msg, args, bot) => {
-        bot.settings.antilink = !bot.settings.antilink;
-        return `✅ Anti-link is now ${bot.settings.antilink ? "ON" : "OFF"}`;
-    }
-};
