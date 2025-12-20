ed64afea065e7c01406770a61d12c7e4856175f4
𝕸𝖊𝖗𝖈𝖍𝖆𝖓𝖙 <isaac0maina@gmail.com>
2025-12-20 10:41:12 +0300

    Create textstyles.js

---
+++ commands/textstyles.js
@@ -0,0 +1,68 @@
+module.exports = {
+
+    // =======
+    purple: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Purple-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    neon: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Neon-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    noel: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Noel-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    metallic: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Metallic-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    devil: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Devil-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    impressive: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Impressive-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    snow: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Snow-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    water: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Water-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    thunder: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Thunder-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    ice: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Ice-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    matrix: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Matrix-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    silver: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Silver-style text generated:\n${args.join(" ")}`;
+    },
+
+    // =======
+    light: async (msg, args, bot) => {
+        return `🅰️ *TOXIC.a.n.t MD* | Light-style text generated:\n${args.join(" ")}`;
+    }
+
+};
